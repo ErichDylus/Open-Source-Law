c8ebb02172812cf5a58da1f77a230dbf158044a9
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-05-29 11:34:44 -0400

    Update stablecoinSplitter.sol

--- solidity/stablecoinSplitter.sol
+++ solidity/stablecoinSplitter.sol
@@ -8,6 +8,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contr
 //FOR DEMONSTRATION ONLY, not recommended to be used for any purpose and carries no warranty of any kind
 //@dev create a stablecoin splitter to avoid $10,000 reporting threshold other than initial transaction to splitterAddress
 //consider wrapping splitterAddress in an on-chain LLC or other vehicle for reporting purposes (perhaps series/Ricardian, see: https://github.com/lexDAO/Ricardian/blob/main/contracts/RicardianLLC.sol)
+// TODO: implement ERC20 interface
 
 contract stableSplitter {
   
@@ -30,10 +31,10 @@ contract stableSplitter {
   //sender inputs token address of stablecoin, lump sum amount, ultimate destination recipient address
   constructor(address _stablecoin, uint256 _amount, address payable _recipient) public payable {
       sender = msg.sender;
-      amount = _amount;
+      amount = _amount * 10e18;
       stablecoin = _stablecoin;
       recipient = _recipient;
-      batches = amount / 10000;
+      batches = _amount * 10e14;
       whitelist[sender] = true;
       whitelist[splitterAddress] = true;
       ERC20(stablecoin).approve(sender, amount); 
@@ -45,7 +46,7 @@ contract stableSplitter {
   //for a new transfer, owner may change recipient address and/or stablecoin address to use this splitter for different tokens
   function newTransfer(address _stablecoin, uint256 _amount, address payable _recipient) public restricted {
       stablecoin = _stablecoin;
-      amount = _amount;
+      amount = _amount * 10e18;
       recipient = _recipient;
       ERC20(stablecoin).approve(sender, amount); 
       ERC20(stablecoin).approve(splitterAddress, amount);
@@ -68,10 +69,10 @@ contract stableSplitter {
             }
             else if (ERC20(stablecoin).balanceOf(splitterAddress) >= 10000 && ERC20(stablecoin).balanceOf(splitterAddress) > 0) {
                 ERC20(stablecoin).transferFrom(splitterAddress, recipient, ERC20(stablecoin).balanceOf(splitterAddress));
-                break
+                break;
             }
             else { break; }
         }
       emit FundsSent();
-  }
+  } 
 }
