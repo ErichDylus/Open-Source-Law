3af5f7c2cd916aaa02b40a35a343bd90cb0ea4a0
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-06-06 12:31:30 -0400

    Update taxWithholding.sol

--- solidity/taxWithholding.sol
+++ solidity/taxWithholding.sol
@@ -4,10 +4,9 @@ pragma solidity ^0.8.0;
 
 /* FOR DEMONSTRATION ONLY, not recommended to be used for any purpose and provided with no warranty whatsoever
  * Rinkeby testing
- * @dev split your own lump sum income payments into withheld tax and net income
+ * @dev split gross income payments into withheld tax and net income and permit payees to claim their own tax and net income amounts to chosen wallets
  * send portion to tax withholding wallet and remainder to 'checking account wallet'
- * provides rough estimate, division intricacies in process, to be adapted for multiple payees
- * TODO: correct paystubNumber issue
+ * TODO: better math
  */
  
 interface IERC20 {
@@ -32,8 +31,13 @@ interface customDripToken {
 
 contract TaxWithholding {
     IERC20 public ierc20; 
+    uint256 grossIncome;
+    uint256 grossTaxes;
+    uint256 netIncome;
+    uint256 taxDiv;
+    uint256 payees;
     address tokenAddress;
-    address payable[] payees;
+    address payable[] payeeAddress;
     address payable immutable dripDropFactory = payable(0x01f39bad34F5Ab1f601766e3AfA90B2b89114024); // LexDAO MemberDripDropFactory Rinkeby contract address
     mapping(address => uint256) payeeNumber;
     mapping(uint256 => mapping(address => uint256)) paystubNumber;
@@ -41,16 +45,26 @@ contract TaxWithholding {
     
     constructor() payable {}
     
-    //payee submits income in USD, tax rate, stablecoin token address, tax and checking wallet addresses, and chosen ID number
-    function withholdTax(uint256 _income, uint8 _taxRate, address _tokenAddress, address payable _taxWallet, address payable _checkingWallet, uint256 _IDnumber) external returns(uint256, uint256) {
-        require(_taxWallet != address(0) && _checkingWallet != address(0), "Submit valid tax and checking wallet addresses");
-        require(_taxRate > 0 && _taxRate < 100, "Submit tax rate percentage as whole number, for example 25"); // to be updated for more exact percentage calcs
+    // grantor/payor/employer submits gross income in USD, total effective tax rate, number of payees, and stablecoin token address
+    function submitPayroll(uint256 _income, uint8 _taxRate, uint256 _payees, address _tokenAddress) external returns(uint256, uint256) {
+        require(_taxRate > 0 && _taxRate < 100, "Submit tax rate percentage as whole (uint) number, for example 25 for 25%"); // to be updated for more exact percentage calcs
         tokenAddress = _tokenAddress;
+        payees = _payees;
         ierc20 = IERC20(tokenAddress);
-        ierc20.transfer(address(this), _income); // send gross income to this contract assuming 18 decimals
-        uint256 _taxes = (uint256(_income/uint256(_taxRate))) * 10e18;
-        uint256 _afterTaxAmount = (_income - _taxes) * 10e18;
-		ierc20.transferFrom(address(this), _taxWallet, _taxes);
+        taxDiv = uint256(100/_taxRate); // imprecise math, will be updated
+        grossIncome = _income * 10e18;
+        ierc20.transfer(address(this), grossIncome); // send gross income to this contract assuming 18 decimals
+        grossTaxes = (grossIncome/taxDiv); // aggregate tax amount
+        netIncome = grossIncome - grossTaxes; // aggregate net income amount
+        return(grossTaxes, netIncome);
+    }
+    
+    // have grantee/payee/employee submit their checking and tax wallets and claim respective payments
+    function claimIncomeAndTax(address payable _taxWallet, address payable _checkingWallet, uint256 _IDnumber) external returns(uint256, uint256) {
+        require(_taxWallet != address(0) && _checkingWallet != address(0), "Submit valid tax and checking wallet addresses");
+        uint256 _taxes = grossTaxes/payees;
+        uint256 _afterTaxAmount = netIncome/payees;
+        ierc20.transferFrom(address(this), _taxWallet, _taxes);
 		ierc20.transferFrom(address(this), _checkingWallet, _afterTaxAmount);
 		uint256 _taxBalance = ierc20.balanceOf(_taxWallet) / 10e18; 
 		uint256 _checkingBalance = ierc20.balanceOf(_checkingWallet) / 10e18;
