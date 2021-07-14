2346359dcd487f3a28c8dbbe21db47d23c67b762
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-07-14 18:01:36 -0400

    Create AccessByERC20.sol

---
+++ solidity/AccessByERC20.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+// FOR DEMONSTRATION ONLY, unaudited, not recommended to be used for any purpose, carries absolutely no warranty of any kind
+// @dev ERC20 holder-gated access to an IPFS link 
+
+pragma solidity ^0.8.6;
+
+interface ERC20 { 
+    function balanceOf(address account) external view returns (uint256); 
+}
+
+contract AccessByERC20 {
+
+    address token;
+    address owner;
+    string IPFSlink;
+    ERC20 public erc20; // public address of token necessary to access link
+    mapping(address => uint256) tokenBalance;
+    
+    event LinkChanged();
+    
+    // deployer sets token address 
+    constructor(address _token) { 
+        token = _token;
+        erc20 = ERC20(token);
+        owner = msg.sender;
+    }
+    
+    // owner sets IPFS link (and is able to update/change it)
+    function setIPFSLink(string memory _IPFSlink) external {
+        require(msg.sender == owner, "Not authorized to change IPFS link.");
+        IPFSlink = _IPFSlink;
+        emit LinkChanged();
+    }
+    
+    //check msg.sender's token balance and assign mapping in order to accessLink()
+    function setBalance() external {
+        uint256 _balance = _checkBalance(msg.sender);
+        tokenBalance[msg.sender] = _balance;
+    }
+    
+    function _checkBalance(address _caller) internal view returns(uint256) {
+        return(erc20.balanceOf(_caller));
+    }
+    
+    function accessLink() external view returns(string memory) {
+        require(tokenBalance[msg.sender] > 0, "Only tokenholders may access the link. Call setBalance() before trying to access.");
+        return(IPFSlink);
+    } 
+}
