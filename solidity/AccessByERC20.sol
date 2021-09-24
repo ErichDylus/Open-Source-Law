e442a0099b647c66a05426a1e3ce64daefc10f96
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-09-24 11:36:22 -0400

    Update AccessByERC20.sol

--- solidity/AccessByERC20.sol
+++ solidity/AccessByERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // FOR DEMONSTRATION ONLY, unaudited, not recommended to be used for any purpose, carries absolutely no warranty of any kind
-// @dev ERC20 holder-gated access to an IPFS link 
-// future features could include other token standards, threshold amounts, privacy solutions, staked tokens (to permit governance staker-gated access), accessing something other than a string (since mintgate addresses gated links)
+// @dev ERC20 holder-gated access to an IPFS link, for example a governance "stake token"
+// future features could include other token standards (such as NFTs), threshold amounts, privacy solutions, staked tokens (to permit governance staker-gated access), accessing something other than a string (since mintgate addresses gated links)
 
 pragma solidity ^0.8.6;
 
@@ -13,38 +13,29 @@ contract AccessByERC20 {
 
     address token;
     address owner;
-    string IPFSlink;
-    ERC20 public erc20; 
-    mapping(address => uint256) tokenBalance;
+    string IPFShash;
+    ERC20 public ierc20; 
     
-    event LinkChanged();
+    event IPFSChanged();
     
-    // deployer sets token address necessary to access link
+    // deployer sets token address necessary to access IPFS info
+    // @param _token: token address for ERC20 used to gate access
     constructor(address _token) { 
         token = _token;
-        erc20 = ERC20(token);
+        ierc20 = ERC20(token);
         owner = msg.sender;
     }
     
-    // owner sets IPFS link (and is able to update/change it)
-    function setIPFSLink(string memory _IPFSlink) external {
-        require(msg.sender == owner, "Not authorized to change IPFS link.");
-        IPFSlink = _IPFSlink;
-        emit LinkChanged();
+    // owner sets IPFS hash (and is able to update/change it)
+    // @param _IPFShash: IPFS hash of information to be revealed only to ERC20 holder
+    function setIPFShash(string memory _IPFShash) external {
+        require(msg.sender == owner, "Not authorized to change IPFS hash.");
+        IPFShash = _IPFShash;
+        emit IPFSChanged();
     }
     
-    //check msg.sender's token balance and assign mapping in order to accessLink()
-    function setBalance() external {
-        uint256 _balance = _checkBalance(msg.sender);
-        tokenBalance[msg.sender] = _balance;
-    }
-    
-    function _checkBalance(address _caller) internal view returns(uint256) {
-        return(erc20.balanceOf(_caller));
-    }
-    
-    function accessLink() external view returns(string memory) {
-        require(tokenBalance[msg.sender] > 0, "Only tokenholders may access the link. Call setBalance() before trying to access.");
-        return(IPFSlink);
+    function accessIPFShash() external view returns(string memory) {
+        require(ierc20.balanceOf(msg.sender) > 0, "Only tokenholders may access.");
+        return(IPFShash);
     } 
 }
