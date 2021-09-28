0c61981f19c6738a42abae603a5e89aef1112550
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-09-28 15:46:51 -0400

    Update InstallmentERC20Escrow.sol

--- solidity/InstallmentERC20Escrow.sol
+++ solidity/InstallmentERC20Escrow.sol
@@ -1,12 +1,11 @@
 //SPDX-License-Identifier: MIT
 //INCOMPLETE
-
 pragma solidity 0.8.6;
 
 /*unaudited and for demonstration only, subject to all disclosures, licenses, and caveats of the open-source-law repo
-**@dev create a simple smart escrow contract for retainer/escrowed atty-client (or other consultant) representation, with an ERC20 stablecoin as payment, expiration denominated in seconds
-**NON-REFUNDED DEPOSIT except if both parties are ready to close but contract expires before closeRepresentation() called
-**intended to be deployed by client (as funds are placed in escrow upon deployment, and returned to deployer if expired but mutually ready to close)
+**@dev create a simple smart escrow contract for retainer/escrowed service work or client representation, with an ERC20 stablecoin as payment, expiration denominated in seconds
+**NON-REFUNDED DEPOSIT except if both parties are ready to disburse installment but contract expires before closeRepresentation() called
+**intended to be deployed by client (as funds are placed in escrow upon deployment, and returned to deployer if expired but mutually ready to close). Three equal installment amounts.
 **may be forked/altered for number of installments and breakdown of each amount, retainer refundability, etc. */
 
 interface IERC20 { 
@@ -20,16 +19,18 @@ contract InstallmentEscrow {
     
   address escrowAddress;
   address payable client;
-  address payable attorney;
+  address payable servicer;
   address stablecoin;
   uint256 retainer;
   uint256 effectiveTime;
   uint256 expirationTime;
-  bool attorneyApproved;
+  bool servicerApproved;
   bool clientApproved;
-  bool attorneyApproved2;
+  bool firstInstallmentPaid;
+  bool servicerApproved2;
   bool clientApproved2;
-  bool attorneyApproved3;
+  bool secondInstallmentPaid;
+  bool servicerApproved3;
   bool clientApproved3;
   bool isExpired;
   bool isClosed;
@@ -37,6 +38,8 @@ contract InstallmentEscrow {
   string description;
   mapping(address => bool) public parties; //map whether an address is a party for restricted() modifier 
   
+  event firstMilestoneCompleted(uint256 firstMilestoneTime);
+  event secondMilestoneCompleted(uint256 secondMilestoneTime);
   event Expired(bool isExpired);
   event Closed(bool isClosed, uint256 effectiveTime); //event provides exact blockstamp time of payment 
   
@@ -45,40 +48,41 @@ contract InstallmentEscrow {
     _;
   }
   
-  //deployer (client) initiates escrow with description, retainer amount in USD, address of stablecoin, seconds until expiry, and designate recipient attorney
-  //DEPLOYER MUST SEPARATELY APPROVE (by interacting with the ERC20 contract in question's approve()) this contract address for the retainer amount (keep decimals in mind)
+  //deployer (client) initiates escrow with description, retainer amount in USD, address of stablecoin, seconds until expiry, and designate recipient servicer
   // @param: _description should be a brief identifier - perhaps as to parties/underlying asset/documentation reference/hash 
   // @param: _retainer is the total retainer amount which will be deposited in the smart escrow contract
-  // @param: _seller is the attorney's address, who will receive the retainer in installments
+  // @param: _servicer is the servicer's address, who will receive the retainer in installments; in practice this could be a consultant or attorney
   // @param: _stablecoin is the token contract address for the stablecoin to be sent as retainer
   // @param: _secsUntilExpiration is the number of seconds until expiry, which can be converted to days for front end input or the code can be adapted accordingly
-  constructor(string memory _description, uint256 _retainer, address payable _attorney, address _stablecoin, uint256 _secsUntilExpiration) payable {
-      require(_attorney != msg.sender, "Designate different party as attorney");
+  constructor(string memory _description, uint256 _retainer, address payable _servicer, address _stablecoin, uint256 _secsUntilExpiration) payable {
+      require(_servicer != msg.sender, "Designate different party as servicer/service provider");
       client = payable(address(msg.sender));
       retainer = _retainer;
       escrowAddress = address(this);
       stablecoin = _stablecoin;
       ierc20 = IERC20(stablecoin);
       description = _description;
-      attorney = _attorney;
+      servicer = _servicer;
       parties[msg.sender] = true;
-      parties[_attorney] = true;
+      parties[_servicer] = true;
       parties[escrowAddress] = true;
       expirationTime = block.timestamp + _secsUntilExpiration;
   }
   
+  // ********** DEPLOYER MUST SEPARATELY APPROVE (by interacting with the ERC20 contract in question's approve()) this contract address for the retainer amount (keep decimals in mind) ************
+  
   // client may confirm recipient address as extra security measure or change address
-  function designateAttorney(address payable _attorney) public restricted {
-      require(_attorney != attorney, "Address already designated as attorney");
-      require(_attorney != client, "Client cannot also be attorney");
-      require(!isExpired, "Too late to change attorney");
-      parties[_attorney] = true;
-      attorney = _attorney;
+  function designateAttorney(address payable _servicer) public restricted {
+      require(_servicer != servicer, "Address already designated as servicer");
+      require(_servicer != client, "Client cannot also be servicer");
+      require(!isExpired, "Too late to change servicer");
+      parties[_servicer] = true;
+      servicer = _servicer;
   }
   
   // client deposits in escrowAddress
   function sendRetainer() public restricted returns(bool, uint256) {
-      require(clientApproved, "Client should call the applicable installmentComplete() before sending funds"); // safety mechanism to prevent instance where attorney (but not client) is ready to close and funds are in escrow, which would send funds to attorney upon expiry if isExpired == true
+      require(clientApproved, "Client should call the applicable installmentComplete() before sending funds"); // safety mechanism to prevent instance where servicer (but not client) is ready to close and funds are in escrow, which would send funds to servicer upon expiry if isExpired == true
       ierc20.transferFrom(client, escrowAddress, retainer);
       return (true, ierc20.balanceOf(escrowAddress));
       
@@ -90,21 +94,15 @@ contract InstallmentEscrow {
       return (true, ierc20.balanceOf(escrowAddress));
   }
   
-  //escrowAddress sends retainer to attorney
-  function payAttorney() internal returns(bool, uint256) {
-      ierc20.transfer(attorney, retainer);
-      return (true, ierc20.balanceOf(escrowAddress));
-  } 
-  
-  //check if expired, and if so, return balance to client only if attorney is not ready to close, otherwise non-refundable if client fails to approve closing after sending funds
+  //check if expired, and if so, return balance to client only if servicer is not ready to close, otherwise non-refundable if client fails to approve closing after sending funds
   //but, see sendDeposit()'s require statement safety mechanism requiring client to approve closing before sending funds via this contract)
   function checkIfExpired() public returns(bool){
-        if (expirationTime <= uint256(block.timestamp) && !attorneyApproved) {
+        if (expirationTime <= uint256(block.timestamp) && !servicerApproved) {
             isExpired = true;
             returnRetainer(); 
             emit Expired(isExpired);
-        } else if (expirationTime <= uint256(block.timestamp) && attorneyApproved == true) {
-            ierc20.transfer(attorney, retainer);
+        } else if (expirationTime <= uint256(block.timestamp) && servicerApproved == true) {
+            ierc20.transfer(servicer, retainer);
             isExpired = true;
             emit Expired(isExpired);
         } else {
@@ -113,61 +111,83 @@ contract InstallmentEscrow {
         return(isExpired);
     }
     
-  // for attorney to easily check if retainer is in escrowAddress
+  // for servicer to easily check if retainer is in escrowAddress
   function checkEscrow() public restricted view returns(uint256) {
       return ierc20.balanceOf(escrowAddress);
   }
 
-  // if client wishes to initiate dispute over attorney breach of off chain agreement or repudiate, simply may wait for expiration without sending deposit nor calling this function
-  // for attorney and client to call when first deliverable/milestone/hour threshold is complete
-  function FirstInstallmentComplete() external restricted returns(string memory){
-         if (msg.sender == attorney) {
-            attorneyApproved = true;
-            return("Attorney is ready to close.");
+  // if client wishes to initiate dispute over servicer breach of off chain agreement or repudiate, simply may wait for expiration without sending deposit nor calling this function
+  // for servicer and client to call when first deliverable/milestone/hour threshold is complete
+  function firstMilestoneComplete() external restricted returns(string memory){
+         if (msg.sender == servicer) {
+            servicerApproved = true;
+            return("Servicer confirms first milestone is complete.");
         } else if (msg.sender == client) {
             clientApproved = true;
-            return("Client is ready to close.");
+            return("Client confirms first milestone is complete.");
         } else {
-            return("You are neither client nor attorney.");
+            return("You are neither client nor servicer.");
         }
   }
   
-  function SecondInstallmentComplete() external restricted returns(string memory){
-         if (msg.sender == attorney) {
-            attorneyApproved2 = true;
-            return("Attorney is ready to close.");
+  //escrowAddress sends first installment to servicer
+  function payFirstInstallment() external restricted returns(bool, uint256) {
+      require(!firstInstallmentPaid, "Already paid first installment.");
+      require (servicerApproved && clientApproved, "Servicer and client must call FirstMilestoneComplete().");
+      ierc20.transfer(servicer, retainer/3);
+      firstInstallmentPaid = true;
+      emit firstMilestoneCompleted(block.timestamp);
+      return (true, ierc20.balanceOf(escrowAddress));
+  } 
+  
+  function secondMilestoneComplete() external restricted returns(string memory){
+        require(servicerApproved && clientApproved, "First Milestone is not complete.");
+        if (msg.sender == servicer) {
+            servicerApproved2 = true;
+            return("Servicer confirms second milestone is complete.");
         } else if (msg.sender == client) {
             clientApproved2 = true;
-            return("Client is ready to close.");
+            return("Client confirms second milestone is complete.");
         } else {
-            return("You are neither client nor attorney.");
+            return("You are neither client nor servicer.");
         }
   }
   
-  function ThirdInstallmentComplete() external restricted returns(string memory){
-         if (msg.sender == attorney) {
-            attorneyApproved3 = true;
-            return("Attorney is ready to close.");
+  //escrowAddress sends second installment to servicer
+  function paySecondInstallment() external restricted returns(bool, uint256) {
+      require(!secondInstallmentPaid, "Already paid second installment.");
+      require (servicerApproved2 && clientApproved2, "Servicer and client must call SecondMilestoneComplete().");
+      ierc20.transfer(servicer, retainer/3);
+      secondInstallmentPaid = true;
+      emit secondMilestoneCompleted(block.timestamp);
+      return (true, ierc20.balanceOf(escrowAddress));
+  } 
+  
+  function thirdMilestoneComplete() external restricted returns(string memory){
+        require(servicerApproved2 && clientApproved2, "Second Milestone is not complete.");
+        if (msg.sender == servicer) {
+            servicerApproved3 = true;
+            return("Servicer confirms third milestone is complete.");
         } else if (msg.sender == client) {
             clientApproved3 = true;
-            return("Client is ready to close.");
+            return("Client confirms third milestone is complete.");
         } else {
-            return("You are neither client nor seller.");
+            return("You are neither client nor servicer.");
         }
   }
     
-  // checks if both client and attorney are ready to close and expiration has not been met; if so, escrowAddress pays out retainer to attorney
+  // checks if both client and servicer are ready to close representation and expiration has not been met; if so, escrowAddress pays out remainder of retainer to servicer
   // if properly closes, emits event with effective time of closing
   function closeRepresentation() public returns(bool){
-      require(attorneyApproved && clientApproved, "Parties are not ready to close.");
+      require(servicerApproved3 && clientApproved3, "Third Milestone is not complete.");
       if (expirationTime <= uint256(block.timestamp)) {
             isExpired = true;
             returnRetainer(); // see comment above as to deposit refund for accidental expiration, optional/subject to negotiation of parties
             emit Expired(isExpired);
         } else {
+            ierc20.transfer(servicer, ierc20.balanceOf(escrowAddress));
             isClosed = true;
-            payAttorney();
-            effectiveTime = uint256(block.timestamp); // effective time of closing upon payment to attorney
+            effectiveTime = uint256(block.timestamp); // effective time of closing upon payment to servicer
             emit Closed(isClosed, effectiveTime);
         }
         return(isClosed);
