d254784c8599638eac42cbb1d4c7bd22aff62825
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-10-21 13:07:20 -0400

    Update DealStamp.sol

--- solidity/DealStamp.sol
+++ solidity/DealStamp.sol
@@ -4,13 +4,14 @@ pragma solidity 0.8.6;
 
 // unaudited and subject to all disclosures, licenses, and caveats of the open-source-law repo
 // create an on-chain record of a deal's parties, decentralized doc storage ref (which would presumably carry an additional hash layer/other security mechanism for access), and time of closing
+// adaptations/forks might include a record of oracle information used in an on-chain closing, gasless sig verification, dispute resolution details, etc.
 
 contract DealStamp {
     
-  uint256 dealNumber; //ID number for stamped deals
-  DealInformation[] deals; //array of deal info structs
-  mapping(address => uint256 /*dealNumber*/) dealParty; //map whether an address is a party to the indexed deal
-  mapping(uint256 /*dealNumber*/ => string) documentsLocation; //deal doc storage location - may be encrypted on the client side and decrypted after retrieval, or otherwise protected
+  uint256 dealNumber; // ID number for stamped deals
+  DealInformation[] deals; // array of deal info structs
+  mapping(address => uint256 /*dealNumber*/) dealParty; // map whether an address is a party to the enumerated deal
+  mapping(uint256 /*dealNumber*/ => string) documentsLocation; // deal doc storage location - may be encrypted on the client side and decrypted after retrieval, or otherwise protected
  
   struct DealInformation {
       uint256 dealNumber;
@@ -36,10 +37,10 @@ contract DealStamp {
       documentsLocation[dealNumber] = _documentsLocationHash;
       deals.push(DealInformation(dealNumber, _effectiveTime, documentsLocation[dealNumber], _party1, _party2));
       emit DealStamped(dealNumber, _effectiveTime, documentsLocation[dealNumber], _party1, _party2);
-      return(dealNumber);
+      return(dealNumber); // DealStamper should keep a record of the dealNumber and provide to relevant parties
   }
   
-  // @param _dealNumber: provide "ID" dealNumber to view stamped deal information
+  // @param _dealNumber: enter dealNumber to view corresponding stamped deal information
   function viewDeal(uint256 _dealNumber) external view returns (uint256, uint256, string memory, address, address) {
       return (deals[_dealNumber].dealNumber, deals[_dealNumber].effectiveTime, deals[_dealNumber].documentsLocation, deals[_dealNumber].party1, deals[_dealNumber].party2);
   }
