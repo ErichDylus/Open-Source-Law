b3b6ee98754a353abf3f67bc5201a7ef718406e1
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-10-26 16:52:35 -0400

    Update DealStamp.sol
    - changed two-party structure to an array of addresses in the DealInformation struct per LexDAO feedback
    - added ability for a party to a deal to add another party to said deal's array of party addresses, for example if a deal is financed, assigned, etc.

--- solidity/DealStamp.sol
+++ solidity/DealStamp.sol
@@ -11,35 +11,55 @@ contract DealStamp {
   uint256 dealNumber; // ID number for stamped deals
   DealInformation[] deals; // array of deal info structs
   mapping(uint256 /*dealNumber*/ => string) documentsLocation; // deal doc storage location - may be encrypted on the client side and decrypted after retrieval, or otherwise protected
+  mapping(address => bool) isParty;
+  mapping(uint256 => mapping(address => bool)) isPartyToDeal;
  
   struct DealInformation {
       uint256 dealNumber;
       uint256 effectiveTime;
       string documentsLocation; 
-      address party1;
-      address party2;
+      address[] parties;
   }
   
-  event DealStamped(uint256 indexed dealNumber, uint256 effectiveTime, string documentsLocation, address party1, address party2);  
+  event DealStamped(uint256 indexed dealNumber, uint256 effectiveTime, string documentsLocation, address[] parties);  
+  event PartyAdded(uint256 indexed dealNumber, address newParty);
   
   constructor() {
-      deals.push(DealInformation(dealNumber,0,"constructor",address(this),address(this)));
+      address[] memory constructorAddr = new address[](1);
+      constructorAddr[0] = address(this);
+      deals.push(DealInformation(dealNumber,0,"constructor", constructorAddr));
+      isPartyToDeal[dealNumber][address(this)] = true;
   }
   
   // @param _documentsLocationHash: IPFS or other decentralized storage location hash of deal documents
   // @param _effectiveTime: unix time of closing, such as the block.timestamp of a stablecoin transfer or smart escrow contract closing
-  // @param _party1: address of a party to the deal
-  // @param _party2: address of a party to the deal
+  // @param _party1: address of first party to the deal
+  // @param _party2: address of second party to the deal
   function newDealStamp(string calldata _documentsLocationHash, uint256 _effectiveTime, address _party1, address _party2) external returns (uint256) {
       dealNumber++;
       documentsLocation[dealNumber] = _documentsLocationHash;
-      deals.push(DealInformation(dealNumber, _effectiveTime, documentsLocation[dealNumber], _party1, _party2));
-      emit DealStamped(dealNumber, _effectiveTime, documentsLocation[dealNumber], _party1, _party2);
+      address[] memory parties = new address[](2);
+      parties[0] = _party1;
+      parties[1] = _party2;
+      deals.push(DealInformation(dealNumber, _effectiveTime, documentsLocation[dealNumber], parties));
+      emit DealStamped(dealNumber, _effectiveTime, documentsLocation[dealNumber], parties);
+      isPartyToDeal[dealNumber][_party1] = true;
+      isPartyToDeal[dealNumber][_party2] = true;
       return(dealNumber); // DealStamper should keep a record of the dealNumber and provide to relevant parties
   }
   
   // @param _dealNumber: enter dealNumber to view corresponding stamped deal information
-  function viewDeal(uint256 _dealNumber) external view returns (uint256, uint256, string memory, address, address) {
-      return (deals[_dealNumber].dealNumber, deals[_dealNumber].effectiveTime, deals[_dealNumber].documentsLocation, deals[_dealNumber].party1, deals[_dealNumber].party2);
+  function viewDeal(uint256 _dealNumber) external view returns (uint256, uint256, string memory, address[] memory) {
+      return (deals[_dealNumber].dealNumber, deals[_dealNumber].effectiveTime, deals[_dealNumber].documentsLocation, deals[_dealNumber].parties);
+  }
+  
+  // @param _dealNumber: deal number of deal for which the new party will be added
+  // @param _newParty: address of the new party to be added to the deal corresponding to _dealNumber
+  function addPartyToDeal(uint256 _dealNumber, address _newParty) external returns (bool success) {
+      require(isPartyToDeal[_dealNumber][msg.sender] == true, "Must be a party to this deal in order to add a party");
+      isPartyToDeal[_dealNumber][_newParty] = true;
+      deals[_dealNumber].parties.push(_newParty);
+      emit PartyAdded(_dealNumber, _newParty);
+      return (true);
   }
 }
