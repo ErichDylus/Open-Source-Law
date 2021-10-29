c0d0c8f68b798a9ce58f989fd5c51a9d447f1578
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-10-29 14:07:25 -0400

    Update taxWithholding.sol

--- solidity/taxWithholding.sol
+++ solidity/taxWithholding.sol
@@ -17,38 +17,38 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-
+// contingent on numerous external factors such as jurisdiction and status of each msg.sender, but initially created as a flat rate federal tax demonstration
+// meant to be an abstract add-on to another contract which results in income taxable event when a user makes a transaction 
 contract TaxWithholding {
   
-    address payable immutable IRS; 
-    address payable[] taxpayers;
+    address payable immutable IRS;
     address tokenAddress;
-    bool isPaid;
-    uint8 immutable taxRate;
+    address[] taxpayers;
+    uint256 immutable taxRate;
     IERC20 public ierc20;
     mapping(address => bool) paid;
     
+    event TaxWithheld(address indexed taxpayer, uint256 indexed time, uint256 taxes);
+    
     // @param _IRSaddress: IRS's designated address to receive taxes
     // @param _taxRate: flat percentage tax rate expressed as a whole number
-    constructor(address _IRSaddress, uint8 _taxRate) payable {
+    constructor(address _IRSaddress, uint256 _taxRate) payable {
         require(_IRSaddress != address(0), "Submit valid IRS wallet address");
         require(_taxRate > 0 && _taxRate < 100, "Submit tax rate percentage as whole number, for example 5 for 5%");
         IRS = payable(_IRSaddress); 
         taxRate = _taxRate;
     }
     
-    // function approve() {} - consider replacing ERC20 interface approve/transfer with safeTransfers
-    
-    // @param _income:  income amount and 
+    // ******** msg.sender must separately approve address(this) for tokenAddress ********, will need easier implementation
+    // @param _income: received income amount by msg.sender in the applicable token corresponding to _tokenAddress 
     // @param _tokenAddress: contract address of ERC20 token received (if applicable/if not simple ETH payment)
-    function withholdTax(uint256 _income, address _tokenAddress) public returns(uint256, uint256) {
+    function withholdTax(uint256 _income, address _tokenAddress) public returns(uint256, bool) {
         ierc20 = IERC20(_tokenAddress);
-        ierc20.transfer(address(this), _income); // send gross income to this contract
-        uint256 _taxes = (uint256(_income/uint256(taxRate)));
-        uint256 _afterTaxAmount = _income - _taxes;
-		    ierc20.transferFrom(address(this), IRS, _taxes);
-		    ierc20.transferFrom(address(this), msg.sender, _afterTaxAmount);
-		    paid[msg.sender] = true;
-        return(_taxes, _afterTaxAmount);
+        uint256 _taxes = (uint256(_income/taxRate));
+		ierc20.transferFrom(msg.sender, IRS, _taxes);
+		taxpayers.push(msg.sender);
+		paid[msg.sender] = true;
+		emit TaxWithheld(msg.sender, block.timestamp, _taxes);
+        return(_taxes, paid[msg.sender]);
     }
 }
