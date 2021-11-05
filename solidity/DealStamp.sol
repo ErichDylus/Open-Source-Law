9cecb7692b2284e91376b6a34c8f30876669bed7
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-11-05 12:08:42 -0400

    Update DealStamp.sol

--- solidity/DealStamp.sol
+++ solidity/DealStamp.sol
@@ -2,9 +2,11 @@
 
 pragma solidity 0.8.6;
 
-// unaudited and subject to all disclosures, licenses, and caveats of the open-source-law repo
-// create an on-chain record of a deal's parties, decentralized doc storage ref (which would presumably carry an additional hash layer/other security mechanism for access), and time of closing
-// adaptations/forks might include a record of oracle information used in an on-chain closing, gasless sig verification, dispute resolution details, etc.
+/// unaudited and subject to all disclosures, licenses, and caveats of the open-source-law repo
+/// @title DealStamp
+/// @author Erich Dylus
+/// @notice An on-chain record of a deal's parties, decentralized doc storage ref (which would presumably carry an additional hash layer/other security mechanism for access), and time of closing
+/// adaptations/forks might include a record of oracle information used in an on-chain closing, gasless sig verification, dispute resolution details, etc.
 
 contract DealStamp {
     
@@ -31,10 +33,11 @@ contract DealStamp {
       isPartyToDeal[dealNumber][address(this)] = true;
   }
   
-  // @param _documentsLocationHash: IPFS or other decentralized storage location hash of deal documents
-  // @param _effectiveTime: unix time of closing, such as the block.timestamp of a stablecoin transfer or smart escrow contract closing
-  // @param _party1: address of first party to the deal
-  // @param _party2: address of second party to the deal
+  /// @param _documentsLocationHash IPFS or other decentralized storage location hash of deal documents
+  /// @param _effectiveTime unix time of closing, such as the block.timestamp of a stablecoin transfer or smart escrow contract closing
+  /// @param _party1 address of first party to the deal
+  /// @param _party2 address of second party to the deal
+  /// @return returns the dealNumber of the newly stamped deal
   function newDealStamp(string calldata _documentsLocationHash, uint256 _effectiveTime, address _party1, address _party2) external returns (uint256) {
       dealNumber++;
       documentsLocation[dealNumber] = _documentsLocationHash;
@@ -48,13 +51,13 @@ contract DealStamp {
       return(dealNumber); // DealStamper should keep a record of the dealNumber and provide to relevant parties
   }
   
-  // @param _dealNumber: enter dealNumber to view corresponding stamped deal information
+  /// @param _dealNumber enter dealNumber to view corresponding stamped deal information
   function viewDeal(uint256 _dealNumber) external view returns (uint256, uint256, string memory, address[] memory) {
       return (deals[_dealNumber].dealNumber, deals[_dealNumber].effectiveTime, deals[_dealNumber].documentsLocation, deals[_dealNumber].parties);
   }
   
-  // @param _dealNumber: deal number of deal for which the new party will be added
-  // @param _newParty: address of the new party to be added to the deal corresponding to _dealNumber
+  /// @param _dealNumber deal number of deal for which the new party will be added
+  /// @param _newParty address of the new party to be added to the deal corresponding to _dealNumber
   function addPartyToDeal(uint256 _dealNumber, address _newParty) external returns (bool success) {
       require(isPartyToDeal[_dealNumber][msg.sender] == true, "Must be a party to this deal in order to add a party");
       isPartyToDeal[_dealNumber][_newParty] = true;
