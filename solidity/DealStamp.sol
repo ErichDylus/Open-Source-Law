5a3816a54efce4f20b8c503186b709b04b9284f5
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-12-01 10:03:07 -0500

    Update DealStamp.sol

--- solidity/DealStamp.sol
+++ solidity/DealStamp.sol
@@ -1,12 +1,12 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.6;
+pragma solidity 0.8.10;
 
 /// unaudited and subject to all disclosures, licenses, and caveats of the open-source-law repo
 /// @title DealStamp
 /// @author Erich Dylus
-/// @notice An on-chain record of a deal's parties, decentralized doc storage ref (which would presumably carry an additional hash layer/other security mechanism for access), and time of closing
-/// adaptations/forks might include a record of oracle information used in an on-chain closing, gasless sig verification, dispute resolution details, etc.
+/// @notice An on-chain record of a deal's parties, decentralized doc room storage ref (presumably carrying an additional hash layer/other security mechanism for access), and time of closing
+/// adaptations/forks might include a record of oracle information used in an on-chain closing, gasless sig verification, removal of parties from array, dispute resolution details, etc.
 
 contract DealStamp {
     
@@ -37,7 +37,8 @@ contract DealStamp {
   /// @param _effectiveTime unix time of closing, such as the block.timestamp of a stablecoin transfer or smart escrow contract closing
   /// @param _party1 address of first party to the deal
   /// @param _party2 address of second party to the deal
-  /// @return returns the dealNumber of the newly stamped deal
+  /// @return the dealNumber of the newly stamped deal
+  /// intended to be called by escrow or other closing smart contracts, by interface or arbitrary call. Additional parties may be added by _party1 or _party2 via addPartyToDeal()
   function newDealStamp(string calldata _documentsLocationHash, uint256 _effectiveTime, address _party1, address _party2) external returns (uint256) {
       dealNumber++;
       documentsLocation[dealNumber] = _documentsLocationHash;
@@ -48,10 +49,11 @@ contract DealStamp {
       emit DealStamped(dealNumber, _effectiveTime, documentsLocation[dealNumber], parties);
       isPartyToDeal[dealNumber][_party1] = true;
       isPartyToDeal[dealNumber][_party2] = true;
-      return(dealNumber); // DealStamper should keep a record of the dealNumber and provide to relevant parties
+      return(dealNumber); // DealStamper should keep a record of the dealNumber and provide to relevant parties; also emitted in the DealStamped event 
   }
   
   /// @param _dealNumber enter dealNumber to view corresponding stamped deal information
+  /// @return the struct information for the inputted dealNumber
   function viewDeal(uint256 _dealNumber) external view returns (uint256, uint256, string memory, address[] memory) {
       return (deals[_dealNumber].dealNumber, deals[_dealNumber].effectiveTime, deals[_dealNumber].documentsLocation, deals[_dealNumber].parties);
   }
