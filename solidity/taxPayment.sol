be93b9c7d4dabb8e74fe9400a6acff4a48f4b0cf
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2022-04-06 17:07:22 -0400

    Update taxPayment.sol

--- solidity/taxPayment.sol
+++ solidity/taxPayment.sol
@@ -1,49 +1,51 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.10;
+pragma solidity >=0.8.10;
 
-/// FOR DEMONSTRATION ONLY, incomplete, unaudited, not recommended to be used for any purpose and provided with no warranty whatsoever
+/// FOR DEMONSTRATION ONLY, incomplete, unaudited, not recommended to be used for any purpose and provided with no warranty whatsoever pursuant to https://github.com/ErichDylus/Open-Source-Law/blob/main/LICENSE
 /// @notice simple tax remitting contract to give IRS its pound of ERC20 flesh, estimated quarterly tax, etc.; sends to immutable IRS wallet address 
  
 interface IERC20 {
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
 
-// contingent on numerous external factors such as jurisdiction and status of each msg.sender, but initially created as a flat rate federal tax demonstration
+// contingent on numerous external factors such as jurisdiction and status of each msg.sender, but initially created as a whole-number flat rate federal tax demonstration
 // meant to be an abstract add-on to another contract which results in income taxable event when a dApp makes a transaction 
 contract TaxPayment {
   
     address payable public immutable IRS;
     uint256 public immutable taxRate;
-    mapping(address => uint256) taxPaymentNumber;
-    mapping(uint256 => mapping(address => uint256) /*taxPaymentNumber*/) taxPaymentNumberAmount;
+    mapping(address => uint256) taxPaymentNumber; // numbered tax payment for each msg.sender
+    mapping(uint256 => mapping(address => uint256) /*taxPaymentNumber*/) taxPaymentNumberAmount; // tax amount for a given msg.sender's taxPaymentNumber
+
+    error InvalidRate();
     
-    event TaxPaid(address indexed taxpayer, uint256 indexed taxPaymentNumber, uint256 indexed timeOfPayment, uint256 taxPaymentAmount);
+    event TaxPaid(address indexed taxpayer, uint256 indexed taxPaymentNumber, uint256 timeOfPayment, uint256 taxPaymentAmount, address tokenContract);
     
     /// @param _IRSaddress IRS's designated address to receive taxes
-    /// @param _taxRate percentage tax rate expressed as a nonzero uint with 18 decimals/100. 1e18 = 100%, 1e17 = 10%, 1e15 = 0.1%, etc. 
+    /// @param _taxRate whole number percentage tax rate expressed as a nonzero uint < 100. 1 = 1%, 5 = 5%, etc. 
     constructor(address _IRSaddress, uint256 _taxRate) payable {
-        require(_taxRate > 0 && _taxRate < 1e18, "INVALID_TAX_RATE");
+        if (_taxRate == 0 || _taxRate >= 100) revert InvalidRate();
         IRS = payable(_IRSaddress); 
         taxRate = _taxRate;
     }
     
-    /// @notice msg.sender must first separately approve address(this) for tokenAddress for at least _income amount
-    /// @param _income received income amount by msg.sender in the applicable token corresponding to _tokenAddress, assuming 18 decimals 
-    /// @param _tokenAddress contract address of ERC20 token received (if applicable/if not simple ETH payment)
+    /// @notice msg.sender must first separately approve address(this) for _tokenAddress for at least _income amount
+    /// @param _income received income amount by msg.sender in the applicable token corresponding to _tokenAddressin appropriate decimal amount
+    /// @param _tokenAddress contract address of ERC20 token received
     /// @return taxes paid, tax payment number for this msg.sender
-    // TODO: add ETH functionality
     function payTax(uint256 _income, address _tokenAddress) public returns(uint256, uint256) {
+        //below saves 50000 in contract size compared to uint256 _taxes = (_income*taxRate) / 100;
         uint256 _taxes;
         uint256 _rate = taxRate;
-        uint256 _denom = 1e18;
-        assembly {
+        uint256 _denom = 100;
+        assembly { 
             _taxes := div(mul(_income, _rate), _denom)
         }
         IERC20(_tokenAddress).transferFrom(msg.sender, IRS, _taxes);
-	taxPaymentNumber[msg.sender]++;
-	taxPaymentNumberAmount[_taxes][msg.sender] = taxPaymentNumber[msg.sender];
-	emit TaxPaid(msg.sender, taxPaymentNumber[msg.sender], block.timestamp, _taxes);
+	    unchecked { ++taxPaymentNumber[msg.sender]; } // will not overflow on human timelines
+	    taxPaymentNumberAmount[_taxes][msg.sender] = taxPaymentNumber[msg.sender];
+	    emit TaxPaid(msg.sender, taxPaymentNumber[msg.sender], block.timestamp, _taxes, _tokenAddress);
         return(_taxes, taxPaymentNumber[msg.sender]);
     }
 }
