e51530d1a56977e9986d5533ec6a7f775c546bb6
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2022-05-02 10:25:15 -0400

    Update EscrowStablecoin.sol

--- solidity/EscrowStablecoin.sol
+++ solidity/EscrowStablecoin.sol
@@ -1,12 +1,11 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.6;
+pragma solidity >=0.8.10;
 
 /// unaudited and for demonstration only, subject to all disclosures, licenses, and caveats of the open-source-law repo
 /// @title Stablecoin Escrow
-/// @notice bilateral smart escrow contract, with an ERC20 stablecoin as payment, expiration denominated in seconds, deposit refunded if contract expires before closeDeal() called
-/// @notice intended to be deployed by buyer (who must separately approve() the contract address for the deposited funds, and deposit is returned to deployer if expired)
-/// @dev may be altered for separation of deposit from purchase price, deposit non-refundability, different token standard, etc.
+/// @notice bilateral smart escrow contract, with an ERC20 stablecoin as payment, expiration denominated in seconds, deposit refunded if contract expires before closeDeal() called 
+/// @dev intended to be deployed by buyer - may be altered for separation of deposit from purchase price, deposit non-refundability, different token standard, etc.
 
 interface IERC20 { 
     function balanceOf(address account) external view returns (uint256);
@@ -17,8 +16,8 @@ interface IERC20 {
 contract EscrowStablecoin {
     
   address escrowAddress;
-  address payable buyer;
-  address payable seller;
+  address buyer;
+  address seller;
   address stablecoin;
   uint256 deposit;
   uint256 expirationTime;
@@ -48,9 +47,9 @@ contract EscrowStablecoin {
   /// @param _seller is the seller's address, who will receive the purchase price if the deal closes
   /// @param _stablecoin is the token contract address for the stablecoin to be sent as deposit
   /// @param _secsUntilExpiration is the number of seconds until the deal expires, which can be converted to days for front end input or the code can be adapted accordingly
-  constructor(string memory _description, uint256 _deposit, address payable _seller, address _stablecoin, uint256 _secsUntilExpiration) payable {
+  constructor(string memory _description, uint256 _deposit, address _seller, address _stablecoin, uint256 _secsUntilExpiration) payable {
       if (_seller == msg.sender) revert BuyerAddrSameAsSellerAddr();
-      buyer = payable(address(msg.sender));
+      buyer = msg.sender;
       deposit = _deposit;
       escrowAddress = address(this);
       stablecoin = _stablecoin;
@@ -65,15 +64,14 @@ contract EscrowStablecoin {
   
   /// @notice buyer may confirm seller's recipient address as extra security measure or change seller address
   /// @param _seller is the new recipient address of seller
-  function designateSeller(address payable _seller) external restricted {
+  function designateSeller(address _seller) external restricted {
       if (_seller == buyer) revert BuyerAddrSameAsSellerAddr();
       if (isExpired) revert Expired();
       parties[_seller] = true;
       seller = _seller;
   }
   
-  /// ********* DEPLOYER MUST SEPARATELY APPROVE (by interacting with the ERC20 contract in question's approve()) this contract address for the deposit amount (keep decimals in mind) ********
-  /// @notice buyer deposits in escrowAddress after separately ERC20-approving escrowAddress
+  /// @notice buyer deposits in escrowAddress ****** after separately ERC20-approving escrowAddress ****** by calling this function OR simply directly transfers the deposit to escrowAddress
   function depositInEscrow() external returns(bool, uint256) {
       if (msg.sender != buyer) revert NotBuyer();
       ierc20.transferFrom(buyer, escrowAddress, deposit);
@@ -94,7 +92,7 @@ contract EscrowStablecoin {
   } 
   
   /// @notice check if expired, and if so, return balance to buyer 
-  function checkIfExpired() external returns(bool){
+  function checkIfExpired() external returns(bool) {
         if (expirationTime <= block.timestamp) {
             isExpired = true;
             _returnDeposit(); 
@@ -105,13 +103,14 @@ contract EscrowStablecoin {
         return(isExpired);
     }
     
-  /// @notice for seller to check if deposit is in escrowAddress
-  function checkEscrow() external restricted view returns(uint256) {
+  /// @notice to check if deposit is in escrowAddress
+  function checkEscrow() external view returns(uint256) {
       return ierc20.balanceOf(escrowAddress);
   }
 
   /// if buyer wishes to initiate dispute over seller breach of off chain agreement or repudiate, simply may wait for expiration without sending deposit nor calling this function
-  function readyToClose() external restricted returns(string memory){
+  /// @notice seller and buyer each call this when ready to close
+  function readyToClose() external restricted returns(string memory) {
          if (msg.sender == seller) {
             sellerApproved = true;
             return("Seller is ready to close.");
@@ -125,7 +124,7 @@ contract EscrowStablecoin {
     
   /// @notice checks if both buyer and seller are ready to close and expiration has not been met; if so, escrowAddress closes deal and pays seller; if not, deposit returned to buyer
   /// @dev if properly closes, emits event with effective time of closing
-  function closeDeal() public {
+  function closeDeal() external {
       if (!sellerApproved || !buyerApproved) revert NotReadyToClose();
       if (expirationTime <= block.timestamp) {
             isExpired = true;
