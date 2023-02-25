62b9895319050b80969ccb8a97c09597cbd4d004
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2023-02-25 14:19:48 -0500

    ds-test using foundry

--- solidity/tests/OpenOffer.t.sol
+++ solidity/tests/OpenOffer.t.sol
@@ -1,54 +1,35 @@
 //SPDX-License-Identifier: MIT
-/*****
- ***** this code and any deployments of this code are strictly provided as-is;
- ***** no guarantee, representation or warranty is being made, express or implied, as to the safety or correctness of the code
- ***** or any smart contracts or other software deployed from these files,
- ***** in accordance with the disclosures and licenses found here: https://github.com/ErichDylus/Open-Source-Law/blob/main/LICENSE
- ***** this code is not audited, and users, developers, or adapters of these files should proceed with caution and use at their own risk.
- ****/
+pragma solidity ^0.8.12;
 
-pragma solidity >=0.8.16;
+/// @notice import DSTest
+import {DSTest} from "ds-test/test.sol";
 
-import "https://github.com/ErichDylus/Open-Source-Law/blob/main/solidity/OpenOffer.sol";
-import {
-    PRBTest
-} from "https://github.com/paulrberg/prb-test/blob/main/src/PRBTest.sol";
+/// @dev import OpenOffer.sol
+import {OpenOffer} from "src/OpenOffer.sol";
 
-/// @title Open Offer Test
+/// @title OpenOfferTest
+contract OpenOfferTest is DSTest {
+    OpenOffer public testInstance;
 
-contract OpenOfferTest is PRBTest {
-    bytes32 public testHash;
+    string input;
 
-    OpenOffer public contracttest;
-
-    function beforeEach(string calldata _documentText) external {
-        contracttest = new OpenOffer(_documentText);
-        testHash = keccak256(abi.encode(_documentText));
-        return assertEq(contracttest.offerOpen(), true, "offer didn't open");
+    constructor(string memory _input) {
+        testInstance = new OpenOffer(_input);
+        input = _input;
     }
 
-    function checkDocHash() external {
-        return
-            assertEq(
-                testHash,
-                contracttest.documentHash(),
-                "hash doesn't match"
-            );
+    function setUp() external {}
+
+    function testOfferOpen() public {
+        return assertTrue(testInstance.offerOpen());
     }
 
-    function checkRevokeOffer() external {
-        (bool success, ) = address(contracttest).delegatecall(
-            abi.encodeWithSignature("revokeOffer()")
-        );
-        require(success, "call failed");
-        assertEq(contracttest.signedAndOfferAccepted(), true, "accept failed");
-        assertEq(contracttest.offerOpen(), false, "offer did not close");
+    function testVerifyOfferText() public {
+        return assertTrue(testInstance.verifyOfferText(input));
     }
 
-    function checkVerifyOfferText(string calldata _documentText) external {
-        (bool success, ) = address(contracttest).delegatecall(
-            abi.encodeWithSignature("verifyOfferText()", _documentText)
-        );
-        require(success, "call failed");
+    function testAcceptOffer() public {
+        testInstance.acceptOffer();
+        return assertTrue(testInstance.signedAndOfferAccepted());
     }
 }
